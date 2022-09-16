47e22c16aee070b9ad78a494f9dda98d03e1abcf
xhiny <axhiny@163.com>
2022-09-16 11:28:30 +0800

    添加手动清空缓存值功能；更新think-core的版本限制。

--- src/Common/TCliMode.php
+++ src/Common/TCliMode.php
@@ -4,17 +4,18 @@ namespace QscmfApiCommon;
 
 use Think\Log;
 
-if (!IS_CLI)  die('The file can only be run in cli mode!');
-
 trait TCliMode
 {
     protected string $log_file_name;
 
     protected function setLogFileName($log_file_name):void{
+        if (!IS_CLI)  die('The file can only be run in cli mode!');
         $this->log_file_name = $log_file_name;
     }
 
     public function writeErrorLog($message, $need_stdout = false):void{
+        if (empty($this->log_file_name)) E("log_file_name is empty");
+
         $need_stdout && fwrite(STDOUT, $message);
         Log::write($message, '', '', $this->getLogFileFullName());
     }
